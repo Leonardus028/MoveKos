dec0156d52b807efdb4736c497f2a1e6f4292743
Leonardus Kevin <leonardus.kevin@ui.ac.id>
2020-11-08 17:48:13 +0700

    splash screen

--- app/src/main/java/com/example/movekos/MainActivity.kt
+++ app/src/main/java/com/example/movekos/MainActivity.kt
@@ -1,9 +1,9 @@
 package com.example.movekos
 
-import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
 
-class MainActivity: AppCompatActivity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
